e8a818b9536211dadb21ac3d80cdf92d90684942
NCenek2 <ceneknicholas@gmail.com>
2026-05-16 09:19:29 -0400

    Update Files for Docker-Compose on whole app

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://react:3000'], // Allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (e.g., cookies)
+    allowedHeaders: [
+      'Content-Type', // For JSON requests
+      'Authorization', // For Bearer tokens
+      'Accept', // Optional, for API responses
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('/api');
-  await app.listen(8080);
+  // Enable CORS with custom options
+  await app.listen(5000);
 }
 bootstrap();
